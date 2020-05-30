c16f4232cdb55f3c43b5724c874574555aae383f
Jim Wright <jimwright808@gmail.com>
2020-05-30 09:52:17 +0100

    initial matter.js set-up

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
 const { Engine, Render, Runner, World, Bodies, Body, Events } = Matter;
 
-const cellsHorizontal = 6;
-const cellsVertical = 2;
-const width = window.innerWidth * 0.9;
-const height = window.innerHeight * 0.75;
+const cellsHorizontal = 10;
+const cellsVertical = 20;
+const width = window.innerWidth * 0.45;
+const height = window.innerHeight * 0.90;
+
+const unitLengthX = width / cellsHorizontal;
+const unitLengthY = height / cellsVertical;
+
+const engine = Engine.create();
+const { world } = engine;
+const render = Render.create({
+  element: document.body,
+  engine: engine,
+  options: {
+    wireframes: false,
+    width,
+    height
+  }
+});
+Render.run(render);
+Runner.run(Runner.create(), engine);
