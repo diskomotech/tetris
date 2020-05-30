ad63c31f465860a99360a045230144f2971ce600
Jim Wright <jimwright808@gmail.com>
2020-05-30 09:42:47 +0100

    initial file set-up

---
+++ index.js
@@ -0,0 +1,6 @@
+const { Engine, Render, Runner, World, Bodies, Body, Events } = Matter;
+
+const cellsHorizontal = 6;
+const cellsVertical = 2;
+const width = window.innerWidth * 0.9;
+const height = window.innerHeight * 0.75;
